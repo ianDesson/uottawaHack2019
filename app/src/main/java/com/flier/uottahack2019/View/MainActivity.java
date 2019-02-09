2b44bb7660247de573d4e0d349d16b1e402161e9
ianDesson <ian.desson@outlook.com>
2019-02-09 13:47:53 -0500

    Added Camera permission

--- app/src/main/java/com/flier/uottahack2019/View/MainActivity.java
+++ app/src/main/java/com/flier/uottahack2019/View/MainActivity.java
@@ -1,7 +1,9 @@
-package com.flier.uottahack2019;
+package com.flier.uottahack2019.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.flier.uottahack2019.R;
 
 public class MainActivity extends AppCompatActivity {
 
