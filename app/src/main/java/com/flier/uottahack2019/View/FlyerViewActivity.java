4953c1805d03946bc76ac2692f8d051281071de6
yj-wong <yujuewong15@gmail.com>
2019-02-09 22:07:38 -0500

    storage setup

--- app/src/main/java/com/flier/uottahack2019/View/FlyerViewActivity.java
+++ app/src/main/java/com/flier/uottahack2019/View/FlyerViewActivity.java
@@ -1,22 +1,16 @@
 package com.flier.uottahack2019.View;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.flier.uottahack2019.R;
 
-public class MainActivity extends AppCompatActivity {
+public class FlyerViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void btnchange(View view) {
-        Intent intent = new Intent(MainActivity.this, FileViewActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.flyerview);
     }
 }
