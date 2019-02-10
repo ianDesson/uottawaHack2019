21febd60f0d3db8368d64459f21a40ac6c16ef2f
ianDesson <ian.desson@outlook.com>
2019-02-09 22:19:46 -0500

    Stuff about Calendar

--- app/src/main/java/com/flier/uottahack2019/GoogleCloudService/CalendarService.java
+++ app/src/main/java/com/flier/uottahack2019/GoogleCloudService/CalendarService.java
@@ -1,11 +1,15 @@
 package com.flier.uottahack2019.GoogleCloudService;
 
+
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
@@ -14,12 +18,15 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
+import com.google.api.services.calendar.model.EventReminder;
 import com.google.api.services.calendar.model.Events;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -34,8 +41,18 @@ public class CalendarService {
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
     private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    private static final String CREDENTIALS_FILE_PATH = "/app/src/res/credentials.json";
 
+    private com.google.api.services.calendar.Calendar mService = null;
+
+    public CalendarService(GoogleAccountCredential credential) {
+        HttpTransport transport = AndroidHttp.newCompatibleTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        mService = new com.google.api.services.calendar.Calendar.Builder(
+                transport, jsonFactory, credential)
+                .setApplicationName("Google Calendar API Android Quickstart")
+                .build();
+    }
 
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
@@ -52,12 +69,83 @@ public class CalendarService {
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
-    public void addEventToCalendar(String calendarId, Event event) throws IOException, GeneralSecurityException {
+    public void addEventToCalendar(String email, Event event) throws IOException, GeneralSecurityException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
-        event = service.events().insert(calendarId, event).execute();
+        event = service.events().insert(email, event).execute();
+    }
+
+
+    /**
+     * DO NOT USE THIS AS IDK WHAT IT DOES
+     */
+    @Deprecated
+    public static void getEvents() throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // List the next 10 events from the primary calendar.
+        DateTime now = new DateTime(System.currentTimeMillis());
+        Events events = service.events().list("primary")
+                .setMaxResults(10)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute();
+        List<Event> items = events.getItems();
+        if (items.isEmpty()) {
+            System.out.println("No upcoming events found.");
+        } else {
+            System.out.println("Upcoming events");
+            for (Event event : items) {
+                DateTime start = event.getStart().getDateTime();
+                if (start == null) {
+                    start = event.getStart().getDate();
+                }
+                System.out.printf("%s (%s)\n", event.getSummary(), start);
+            }
+        }
+    }
+
+
+    /**
+     * Insert an event into te user's calendar
+     *
+     * @param summary       Event Summary/name
+     * @param des           Event Description
+     * @param startDate     Event start time
+     * @param endDate       Event end time
+     * @throws IOException
+     */
+    void insertEvent(String summary, String des, DateTime startDate, DateTime endDate) throws IOException {
+        Event event = new Event()
+                .setSummary(summary)
+                .setDescription(des);
+        EventDateTime start = new EventDateTime()
+                .setDateTime(startDate)
+                .setTimeZone("America/Toronto");
+        event.setStart(start);
+                EventDateTime end = new EventDateTime()
+                .setDateTime(endDate)
+                .setTimeZone("America/Los_Angeles");
+                event.setEnd(end);
+        EventReminder[] reminderOverrides = new EventReminder[] {
+                new EventReminder().setMethod("email").setMinutes(24 * 60),
+                new EventReminder().setMethod("popup").setMinutes(10),
+        };
+        Event.Reminders reminders = new Event.Reminders()
+                .setUseDefault(false)
+                .setOverrides(Arrays.asList(reminderOverrides));
+        event.setReminders(reminders);
+        String calendarId = "primary";
+        //event.send
+        if(mService!=null)
+            mService.events().insert(calendarId, event).setSendNotifications(true).execute();
     }
 
 }
