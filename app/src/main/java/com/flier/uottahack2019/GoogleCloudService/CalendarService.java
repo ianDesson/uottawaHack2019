8a742ae81bd8b6b4406dc959add6a0be1c0aadbd
ianDesson <ian.desson@outlook.com>
2019-02-09 19:48:34 -0500

    I really don't know

--- app/src/main/java/com/flier/uottahack2019/GoogleCloudService/CalendarService.java
+++ app/src/main/java/com/flier/uottahack2019/GoogleCloudService/CalendarService.java
@@ -25,7 +25,7 @@ import java.util.List;
 
 public class CalendarService {
 
-    private static final String APPLICATION_NAME = "uottaHack2019";
+    private static final String APPLICATION_NAME = "uottawaHack2019";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
@@ -52,35 +52,12 @@ public class CalendarService {
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        // Build a new authorized API client service.
+    public void addEventToCalendar(String calendarId, Event event) throws IOException, GeneralSecurityException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         Calendar service = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
-
-        // List the next 10 events from the primary calendar.
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("primary")
-                .setMaxResults(10)
-                .setTimeMin(now)
-                .setOrderBy("startTime")
-                .setSingleEvents(true)
-                .execute();
-        List<Event> items = events.getItems();
-        if (items.isEmpty()) {
-            System.out.println("No upcoming events found.");
-        } else {
-            System.out.println("Upcoming events");
-            for (Event event : items) {
-                DateTime start = event.getStart().getDateTime();
-                if (start == null) {
-                    start = event.getStart().getDate();
-                }
-                System.out.printf("%s (%s)\n", event.getSummary(), start);
-            }
-        }
+        event = service.events().insert(calendarId, event).execute();
     }
-
 
 }
